4a068ba9966ac5f537aa06e138ed518d0fd8fd26
Scott Conley <scott@14to9.org>
2011-03-24 13:51:07 -0400

    linking to logrotate config file

--- lib/flipstone-deployment/capistrano/log_recipes.rb
+++ lib/flipstone-deployment/capistrano/log_recipes.rb
@@ -4,7 +4,9 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :config, :roles => :app do
       template = File.read(File.join(File.dirname(__FILE__), "logrotate.erb"))
       buffer   = ERB.new(template).result(binding)
-      put buffer, "/etc/logrotate.d/#{application}"
+      logrotate_config_path = "#{shared_path}/system/logrotated.conf"
+      put buffer, logrotate_config_path
+      sudo "ln -s /etc/logrotate.d/#{application} #{logrotate_config_path}"
     end
   end
 end
