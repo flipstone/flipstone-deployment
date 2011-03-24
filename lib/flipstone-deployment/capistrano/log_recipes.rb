e8c0f6cbf482698894bd686e99199f5ee1de2534
Scott Conley <scott@14to9.org>
2011-03-24 13:46:07 -0400

    corrected name error

--- lib/flipstone-deployment/capistrano/log_recipes.rb
+++ lib/flipstone-deployment/capistrano/log_recipes.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :logrotate do
     desc "Create application logrotate configuration file"
     task :config, :roles => :app do
-      template = File.read(File.join(File.dirname(__FILE__), "logrotated.erb"))
+      template = File.read(File.join(File.dirname(__FILE__), "logrotate.erb"))
       buffer   = ERB.new(template).result(binding)
       put buffer, "/etc/logrotate.d/#{application}"
     end
