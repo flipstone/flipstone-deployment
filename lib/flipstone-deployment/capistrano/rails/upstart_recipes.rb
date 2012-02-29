e49dcc106a839d3397969ddfdd1a8297a7e46033
David Vollbracht <david.vollbracht@gmail.com>
2012-02-28 22:56:47 -0500

    Let application override upstart conf file if it wants to

--- lib/flipstone-deployment/capistrano/rails/upstart_recipes.rb
+++ lib/flipstone-deployment/capistrano/rails/upstart_recipes.rb
@@ -1,23 +1,25 @@
 Capistrano::Configuration.instance(:must_exist).load do
- namespace :upstart do
-   desc <<-DESC
-     Push upstart config to /etc/init and install to /etc/init.d
-   DESC
-   task :install_application, :roles => :app do
-     upstart_file_path = "#{shared_path}/system/upstart.conf"
-     template = File.read(File.join(File.dirname(__FILE__), "upstartjob.conf.erb"))
-     buffer   = ERB.new(template).result(binding)
-     put buffer, upstart_file_path
-     sudo "ln -sf #{upstart_file_path} /etc/init/#{application}.conf"
-     sudo "ln -sf /lib/init/upstart-job /etc/init.d/#{application}"
-   end
+  set(:upstart_conf_file) { File.join(File.dirname(__FILE__), "upstartjob.conf.erb") }
 
-   task :reload_configuration, :roles => :app do
-     sudo "initctl reload-configuration"
-   end
- end
+  namespace :upstart do
+    desc <<-DESC
+      Push upstart config to /etc/init and install to /etc/init.d
+    DESC
+    task :install_application, :roles => :app do
+      upstart_file_path = "#{shared_path}/system/upstart.conf"
+      template = File.read upstart_conf_file
+      buffer   = ERB.new(template).result(binding)
+      put buffer, upstart_file_path
+      sudo "ln -sf #{upstart_file_path} /etc/init/#{application}.conf"
+      sudo "ln -sf /lib/init/upstart-job /etc/init.d/#{application}"
+    end
 
- before 'deploy:start', 'upstart:install_application'
- after 'upstart:install_application', 'upstart:reload_configuration'
+    task :reload_configuration, :roles => :app do
+      sudo "initctl reload-configuration"
+    end
+  end
+
+  before 'deploy:start', 'upstart:install_application'
+  after 'upstart:install_application', 'upstart:reload_configuration'
 end
- 
+
